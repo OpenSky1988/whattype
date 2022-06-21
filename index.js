03455434e238f2005dc3cd4bcef13456fb45e2e5
Alexander <mutilatorap@gmail.com>
2022-06-21 17:24:53 -0500

    isPrimitiveType

--- index.js
+++ index.js
@@ -6,3 +6,20 @@
 function isNull(value) {
   return value === null;
 }
+
+/**
+ * Defines whether a value is of a primitive data type. See https://developer.mozilla.org/en-US/docs/Glossary/Primitive
+ * @param {any} value Data of any type
+ * @returns boolean
+ */
+function isPrimitiveType(value) {
+  if (isNull(value)) {
+    return true;
+  }
+
+  if (typeof value === 'object' || typeof value === 'function') {
+    return false;
+  } else {
+    return true;
+  }
+}
